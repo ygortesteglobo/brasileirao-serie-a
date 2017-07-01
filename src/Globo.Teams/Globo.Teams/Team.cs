ab1c7a146ba51a4418e401a612115d342ba5523e
ygorops <ygorsantos92@gmail.com>
2017-07-01 02:18:36 -0300

    inicia projeto

---
+++ src/Globo.Teams/Globo.Teams/Team.cs
@@ -0,0 +1,19 @@
+﻿namespace Globo.Teams
+{
+	using Newtonsoft.Json;
+
+	public class Team
+    {
+		[JsonProperty("id")]
+		public string Id { get; set; }
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("shortName")]
+		public string ShortName { get; set; }
+
+		[JsonProperty("image")]
+		public string Image { get; set; }
+    }
+}
