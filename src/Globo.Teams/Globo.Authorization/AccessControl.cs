4e312eeefd625bcf1f8d29272cf8d306752559ab
ygortesteglobo <ygorsantos92+1@gmail.com>
2017-07-02 13:35:53 -0300

    cria projeto que gerencia a autorizacao

---
+++ src/Globo.Teams/Globo.Authorization/AccessControl.cs
@@ -0,0 +1,13 @@
+﻿namespace Globo.Authorization
+{
+	public class AccessControl
+    {
+		public static AccessControl Instance = new AccessControl();
+
+		public bool HasAccess(string user, string role)
+		{
+			// TODO: FAZER CONTROLE DE ACESSO POR USUÁRIO
+			return true;
+		}
+    }
+}
